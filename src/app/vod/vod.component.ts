f7f2d4b0e8325c0667b471ed10d023fc2105267b
vaibhav-gecj <vaibhavchaudhary14107@gmail.com>
2021-10-01 11:32:40 +0530

    done

--- src/app/vod/vod.component.ts
+++ src/app/vod/vod.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-vod',
   templateUrl: './vod.component.html',
-  styleUrls: ['./vod.component.css']
+  styleUrls: ['./vod.component.css'],
 })
 export class VodComponent implements OnInit {
+  div1: boolean = false;
+  Foods = [
+    { value: 'Maths', viewValue: 'Maths' },
+    { value: 'English', viewValue: 'English' },
+    { value: 'Science', viewValue: 'Science' },
+    { value: 'Hindi', viewValue: 'Hindi' },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  div1Function() {
+    this.div1 = true;
   }
-
 }
